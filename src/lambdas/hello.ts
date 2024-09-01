0a0744603fb9afaee002dbe3eaaf79c04bf4dccf
charmingruby <mrrubycanal3105@gmail.com>
2024-09-01 09:01:15 -0300

    feat: base structure

---
+++ src/lambdas/hello.ts
@@ -0,0 +1,6 @@
+import { Response } from '@/helpers/response'
+import { APIGatewayProxyEventV2 } from 'aws-lambda'
+
+export async function handler(_event: APIGatewayProxyEventV2) {
+  return Response.okResponse('Endpoint hit', { ping: true })
+}
